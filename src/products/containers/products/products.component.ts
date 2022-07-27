1de7df6d64f0b2c75a791a3ddd49ffd2c8e4dfb1
Alistair Mackay <alistair.mackay.work@gmail.com>
2022-07-27 22:20:42 +0100

    finished State composition with Selectors

--- src/products/containers/products/products.component.ts
+++ src/products/containers/products/products.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit, ChangeDetectionStrategy } from "@angular/core";
 
+import { Store } from "@ngrx/store";
+import * as fromStore from "../../store";
+
 import { Pizza } from "../../models/pizza.model";
-import { PizzasService } from "../../services/pizzas.service";
+import { Observable } from "rxjs/Observable";
 
 @Component({
   selector: "products",
@@ -12,20 +15,21 @@ import { PizzasService } from "../../services/pizzas.service";
         <a class="btn btn__ok" routerLink="./new"> New Pizza </a>
       </div>
       <div class="products__list">
-        <div *ngIf="!pizzas?.length">No pizzas, add one to get started.</div>
-        <pizza-item *ngFor="let pizza of pizzas" [pizza]="pizza"> </pizza-item>
+        <div *ngIf="!(pizzas$ | async)?.length">
+          No pizzas, add one to get started.
+        </div>
+        <pizza-item *ngFor="let pizza of pizzas$ | async" [pizza]="pizza">
+        </pizza-item>
       </div>
     </div>
   `,
 })
 export class ProductsComponent implements OnInit {
-  pizzas: Pizza[];
+  pizzas$: Observable<Pizza[]>;
 
-  constructor(private pizzaService: PizzasService) {}
+  constructor(private store: Store<fromStore.ProductsState>) {}
 
   ngOnInit() {
-    this.pizzaService.getPizzas().subscribe((pizzas: Pizza[]) => {
-      this.pizzas = pizzas;
-    });
+    this.pizzas$ = this.store.select(fromStore.getAllPizzas);
   }
 }
